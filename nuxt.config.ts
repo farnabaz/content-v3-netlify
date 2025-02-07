ac41d62baf05c9ee62ada6460b3f1812d32bb2cf
Farnabaz <farnabaz@gmail.com>
2025-02-07 15:07:04 +0100

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+  content: {
+    database: {
+      type: 'sqlite',
+      filename: '/tmp/database.sqlite',
+    }
+  }
+})
